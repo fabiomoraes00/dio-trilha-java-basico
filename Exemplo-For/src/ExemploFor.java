238197c0be7b0ed519e4a3841a619d1396ae08bf
Fabiomoraes <fabiomoraes00@gmail.com>
2024-07-28 12:06:19 -0300

    Exemplos For-Array-While-DoWhile-BreakContinue

---
+++ Exemplo-For/src/ExemploFor.java
@@ -0,0 +1,19 @@
+public class ExemploFor {
+public static void main(String[] args) {
+    
+// Outras estruturas
+
+//estrutura 1
+for(int carneirinhos = 1 ; carneirinhos <=20; carneirinhos ++) {
+    System.out.println(carneirinhos + " - Carneirinho(s)");
+}
+
+
+
+    System.out.println("Jurinhas dormiu");
+}
+
+//for( somente 1x ; deve ser uma expresão boolean; acontecerá a cada final da execução ) { }
+
+}
+
---
+++ Exemplo-For/src/ExemploForArray.java
@@ -0,0 +1,14 @@
+// ExemploFor.java
+public class ExemploForArray {
+    public static void main(String[] args) {
+
+        //em arrays o indice inicia em zero
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+    
+        for (int x=0; x<alunos.length; x++) {
+            System.out.println("O aluno no indice x=" + x + " é " + alunos[x]);
+        }
+    
+    }
+    }
+
